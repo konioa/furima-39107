35af2cdf536338154e8a04c32e8852aecd942b65
konioa <goodsong0715@gmail.com>
2023-03-29 18:30:35 +0900

    ActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
   belongs_to :user
   has_one :order
   has_one_attached :image
