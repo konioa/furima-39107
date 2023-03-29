28690a0ae3ad13a1bdf4c506b0d5336090db04d2
konioa <goodsong0715@gmail.com>
2023-03-29 22:49:10 +0900

    バリテーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,26 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  belongs_to :delivery_cost
+  belongs_to :prefecture
+  belongs_to :shipping_date
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
-
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_cost_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  
   belongs_to :user
-  has_one :order
   has_one_attached :image
 
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
+
+  with_options presence: true do
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: "is out of setting range."},
+                    format: { with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters." }
+  end
 end
