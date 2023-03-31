4663533285f5bd413b8ff9a24694486ee3902b57
konioa <goodsong0715@gmail.com>
2023-03-31 21:07:10 +0900

    商品詳細表示1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,6 +18,10 @@ class ItemsController < ApplicationController
     end
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
   private
   def item_params
     params.require(:item).permit(:title, :description, :category_id, :condition_id, :delivery_cost_id, :prefecture_id, :shipping_date_id, :price, :image).merge(user_id: current_user.id)
