c1a68aaf7462934b879f185856077d97ebe8f111
konioa <goodsong0715@gmail.com>
2023-04-01 20:14:20 +0900

    商品削除3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :authenticate_user!, only: [:new, :edit, :destroy]
   before_action :contributor_confirmation, only: [:edit, :update, :destroy]
 
   def index
@@ -35,12 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy     
-    unless user_signed_in?
-      redirect_to root_path
-    else
-      @item.destroy 
-      redirect_to root_path
-    end
+    @item.destroy
+    redirect_to root_path
   end
 
   private
