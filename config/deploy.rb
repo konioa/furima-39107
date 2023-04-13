4bbec08150ff86c0a9e15a267bfebb6040e13e95
konioa <goodsong0715@gmail.com>
2023-04-13 21:25:31 +0900

    自動デプロイ2

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.17.2'
 
 # Capistranoのログの表示に利用する
-set :application, 'furima-39107'
+set :application, 'Furima'
 
 # どのリポジトリからアプリをpullするかを指定する
 set :repo_url,  'git@github.com:konioa/furima-39107.git'
